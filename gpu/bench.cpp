aed6822b43dbd427a77042afcc292af65f9b3850
jeremy beuvry <jeremy.beuvry@epita.fr>
2019-12-28 21:16:22 +0100

    start of aabb

--- gpu/bench.cpp
+++ gpu/bench.cpp
@@ -1,14 +1,18 @@
 #include <benchmark/benchmark.h>
 
 #include "parser.h"
+#include "partitioning/aabb.h"
 
 #define TESTS_PATH "../../tests/"
 
 // All tests
-#define SIMPLE_CUBE TESTS_PATH "cube.svati"
-#define MEDIUM_ISLAND TESTS_PATH "island_smooth.svati"
-#define COMPLEX_SPHERE TESTS_PATH "spheres.svati"
+#define CUBE TESTS_PATH "cube.svati"
+#define ISLAND_SMOOTH TESTS_PATH "island_smooth.svati"
+#define SPHERES TESTS_PATH "spheres.svati"
 
+/*
+ * Benchmark the parser to see memory alignement difference
+ */
 template <class ...ExtraArgs>
 void BM_parser(benchmark::State& st, ExtraArgs&&... extra_args)
 {
@@ -16,9 +20,26 @@ void BM_parser(benchmark::State& st, ExtraArgs&&... extra_args)
     parser(std::forward<ExtraArgs>(extra_args)...);
 }
 
-BENCHMARK_CAPTURE(BM_parser, simple_cube, SIMPLE_CUBE);
-BENCHMARK_CAPTURE(BM_parser, medium_island, MEDIUM_ISLAND);
-BENCHMARK_CAPTURE(BM_parser, complex_sphere, COMPLEX_SPHERE);
+//BENCHMARK_CAPTURE(BM_parser, simple_cube, CUBE);
+//BENCHMARK_CAPTURE(BM_parser, medium_island, ISLAND_SMOOTH);
+//BENCHMARK_CAPTURE(BM_parser, complex_sphere, SPHERES);
+
+/*
+ * Benchmark the AABB creation with the object strategy
+ */
+void BM_aabb_object(benchmark::State& st)
+{
+  struct scene scene = parser(SPHERES);
+  struct scene cuda_scene = to_cuda(&scene);
+
+  struct AABB *aabbs;
+  cudaMalloc(&aabbs, sizeof(struct AABB) * cuda_scene.object_count);
+
+  for (auto _ : st)
+    compute_bounding_box(&cuda_scene, aabbs);
+}
+
+BENCHMARK(BM_aabb_object);
 
 
 BENCHMARK_MAIN();
